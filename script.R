018f66a743b18a0c5cc52d42ee3bd7168c7afa78
michbur <michalburdukiewicz@gmail.com>
2017-10-08 23:34:14 +0200

    updated presentation

--- script.R
+++ script.R
@@ -12,7 +12,7 @@ plot_pca <- function(pca)
   stat_density2d(aes(fill=taxon, alpha=..level..), 
                  color = "black", contour = TRUE, geom="polygon") +
   scale_linetype_discrete("") +
-  scale_fill_manual("", values = c("orange", "blue"), labels = c("Inne eukarioty", "Plasmodiidae")) +
+  scale_fill_manual("", values = c("orange", "blue"), labels = c("Other eukaryotes", "Plasmodiidae")) +
   scale_shape_discrete("") +
   scale_color_discrete("") +
   scale_alpha_continuous(range = c(0.25, 0.4)) +
@@ -48,10 +48,10 @@ p3 <- ggplot(cmp_dat, aes(x = MCC, y = MCC_p)) +
   geom_point(aes(fill = sp), size = 2, shape = 21, color = "white") +
   geom_point(aes(color = cs), size = 3, shape = 1, stroke = 1) +
   my_theme +
-  scale_x_continuous("MCC (inne eukarioty)") +
+  scale_x_continuous("MCC (other eukaryotes)") +
   scale_y_continuous("MCC (Plasmodiidae)") +
-  scale_fill_manual("Długość alfabetu\n(peptyd sygnałowy)", values = c("dodgerblue", "firebrick1")) +
-  scale_color_manual("Długość alfabetu\n(miejsce cięcia)", values = c("dodgerblue", "firebrick1")) +
+  scale_fill_manual("Length of the alphabet\n(signal peptide)", values = c("dodgerblue", "firebrick1")) +
+  scale_color_manual("Length of the alphabet\n(cleavage site)", values = c("dodgerblue", "firebrick1")) +
   ggtitle("C")
 
 cairo_ps("./figures/SP.eps", height = 5.4, width = 6.5)
